0e11eabc2cd43c9cdbc2b9f16e38259f302cac22
Hazim Okanovic <hazimokanovic@live.com>
2024-05-19 14:08:35 +0200

    Fixed the events tests

--- tests/dateNightEventsTest.spec.js
+++ tests/dateNightEventsTest.spec.js
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
-import { EventsPage } from "/Users/hazim/Documents/Dandelion_Chocolate/pages/eventsPage";
-import { Data, PagesUrls } from "/Users/hazim/Documents/Dandelion_Chocolate/testData/data";
+import { EventsPage } from "../pages/eventsPage";
+import { Data, PagesUrls } from "../testData/data";
 
 let page;
 let context;
@@ -11,28 +11,28 @@ test.beforeAll(async ({ browser, baseURL }) => {
     await page.goto(PagesUrls.eventsPage);
 });
 
-test('Factory tour events test',async () => {
-    test.setTimeout(8000000);
+test('Dandelion Night events test',async () => {
     await expect(await page.url()).toMatch(PagesUrls.eventsPage)
     let eventsPage = new EventsPage(page);
     let allEvents = [];
-    for (let index = 0; index < await eventsPage.factoryTourEvents.count(); index++) {
-        const element = await eventsPage.factoryTourEvents.nth(index);
+    for (let index = 0; index < await eventsPage.dateNightEvents.count(); index++) {
+        const element = await eventsPage.dateNightEvents.nth(index);
         allEvents.push(element);
     }
-       
+           
     for (const event of allEvents) {
         await event.click();
-        expect(await eventsPage.modalHeader).toContainText(Data.factoryTour);
+        expect(await eventsPage.modalHeader).toHaveText(Data.dateNightHeader);
         const [newPage] = await Promise.all([
             context.waitForEvent('page'), 
             await eventsPage.bookNowButton.click()
             ])
         await newPage.waitForTimeout(13000);
-        await expect(newPage.locator("//div[@class='Consumer-reservationInfo']//h3[contains(text(), 'Tour')]")).toHaveText(Data.factoryTourHeader);
+        await expect(newPage.locator("//div[@class='Consumer-reservationInfo']//h3[contains(text(), 'Date Night')]")).toHaveText(Data.dateNightHeader);
+        await expect(newPage.locator("(//button[@data-testid='booking-card-button'])[1]")).toBeVisible({timeout: 3000});
         await expect(newPage.locator("//div[@class='MuiTypography-root css-i77tdi']")).not.toBeAttached({timeout: 3000});
         await expect(newPage.locator("//div[@class='MuiTypography-root css-i77tdi']")).toBeHidden({timeout: 3000});
-                        
+                
         await newPage.close()
         await eventsPage.clickXModal();
     }
--- tests/truffleMakingEventsTest.spec.js
+++ tests/truffleMakingEventsTest.spec.js
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
-import { EventsPage } from "/Users/hazim/Documents/Dandelion_Chocolate/pages/eventsPage";
-import { Data, PagesUrls } from "/Users/hazim/Documents/Dandelion_Chocolate/testData/data";
+import { EventsPage } from "../pages/eventsPage";
+import { Data, PagesUrls } from "../testData/data";
 
 let page;
 let context;
@@ -11,24 +11,24 @@ test.beforeAll(async ({ browser, baseURL }) => {
     await page.goto(PagesUrls.eventsPage);
 });
 
-test('Chocolate Making events test',async () => {
+test('Truffle making events test',async () => {
     await expect(await page.url()).toMatch(PagesUrls.eventsPage)
     let eventsPage = new EventsPage(page);
     let allEvents = [];
-    for (let index = 0; index < await eventsPage.chocolateMakingEvents.count(); index++) {
-        const element = await eventsPage.chocolateMakingEvents.nth(index);
+    for (let index = 0; index < await eventsPage.truffleMakingEvents.count(); index++) {
+        const element = await eventsPage.truffleMakingEvents.nth(index);
         allEvents.push(element);
     }
        
     for (const event of allEvents) {
         await event.click();
-        expect(await eventsPage.modalHeader).toHaveText(Data.chocolateMakingHeader);
+        expect(await eventsPage.modalHeader).toHaveText(Data.truffleMakingHeader);
         const [newPage] = await Promise.all([
             context.waitForEvent('page'), 
             await eventsPage.bookNowButton.click()
             ])
         await newPage.waitForTimeout(13000);
-        await expect(newPage.locator("//div[@class='Consumer-reservationInfo']//h3[contains(text(), 'Making')]")).toHaveText(Data.chocolateMakingHeader);
+        await expect(newPage.locator("//div[@class='Consumer-reservationInfo']//h3[contains(text(), 'Making')]")).toHaveText(Data.truffleMakingHeader);
         await expect(newPage.locator("(//button[@data-testid='booking-card-button'])[1]")).toBeVisible({timeout: 3000});
         await expect(newPage.locator("//div[@class='MuiTypography-root css-i77tdi']")).not.toBeAttached({timeout: 3000});
         await expect(newPage.locator("//div[@class='MuiTypography-root css-i77tdi']")).toBeHidden({timeout: 3000});
