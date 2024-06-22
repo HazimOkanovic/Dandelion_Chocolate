0f24ed184523be9381e4ecec38e4917026ead4ea
Hazim Okanovic <hazimokanovic@live.com>
2024-06-22 19:34:38 +0200

    update

--- email/reporter.js
+++ email/reporter.js
@@ -5,7 +5,7 @@ let onTestBeginString;
 let onTestEndString;
 let onEndString;
 let allText = []
-exports.MyReporter = class MyReporter {
+class MyReporter {
 
     onBegin(config, suite) {
       onBeginString = `Starting the run with ${suite.allTests().length} tests`
@@ -33,3 +33,4 @@ exports.MyReporter = class MyReporter {
       return allText
     }  
 }
+module.exports = MyReporter
